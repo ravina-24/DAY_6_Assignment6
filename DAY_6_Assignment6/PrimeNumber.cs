ed51b9a1ff2dd6a039b89792e1af4e925038d14a
ravina-24 <ravinamaudekar24@gmail.com>
2021-09-29 23:55:26 +0530

    Add project files.

---
+++ DAY_6_Assignment6/PrimeNumber.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY_6_Assignment6
+{
+    class PrimeNumber
+    {
+
+        public void PrintPrimeNumber()
+        {
+
+            int firstRange;
+            Console.WriteLine("Enter first range = ");
+            firstRange = Convert.ToInt32(Console.ReadLine());
+
+
+            int secondRange;
+            Console.WriteLine("Enter second range = ");
+            secondRange = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Prime number between " + firstRange + "and " + secondRange + "are :");
+
+
+            while (firstRange < secondRange)
+            {
+                int flag = 0;
+                int i = 2;
+                while(i<firstRange)
+                {
+
+                
+                    if (firstRange % i == 0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                    i++;
+                }
+                if (flag == 0 && firstRange!=1 && firstRange!=0)
+                {
+
+                    Console.WriteLine(firstRange);
+                }
+
+                firstRange++;
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
---
+++ DAY_6_Assignment6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DAY_6_Assignment6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrimeNumber p = new PrimeNumber();
+            p.PrintPrimeNumber();
+
+        }
+    }
+}
