035d48bbcd8a4e2fc3d0b586dbd4f14d136226e3
ravina-24 <ravinamaudekar24@gmail.com>
2021-10-04 00:14:42 +0530

    [Rvaina] Added : Method to find the repeated number in an array.

---
+++ DAY_6_Assignment6/RepeatedNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY_6_Assignment6
+{
+    class RepeatedNumber
+    {
+        public void FindRepeatedNumber()
+        {
+            int length;
+            Console.WriteLine("Enter size of an array = ");
+            length = Convert.ToInt32(Console.ReadLine());
+
+            int[] numbers = new int[length];
+
+            Console.WriteLine("Enter Numbers = ");
+            for(int i=0;i<length;i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Repeated elements are ");
+            for(int i=0;i<length;i++)
+            {
+                for (int j = i+1; j < length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        Console.WriteLine(numbers[j]);
+                        break;
+                    }
+                }        
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
